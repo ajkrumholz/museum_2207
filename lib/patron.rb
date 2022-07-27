18c9410c919bf0552b74d09368e830250e58314d
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-27 10:25:12 -0400

    add #add_interests

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,2 +1,13 @@
 class Patron
+  attr_reader :name, :spending_money, :interests
+
+  def initialize(name, spending_money)
+    @name = name
+    @spending_money = spending_money
+    @interests = []
+  end
+
+  def add_interests(new_interest)
+    @interests << new_interest
+  end
 end
