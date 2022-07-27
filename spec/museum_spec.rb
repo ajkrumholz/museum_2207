6b3f59d72a8e8e3100d484362065519503713237
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-27 10:37:35 -0400

    add tests for #recommend_exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Museum do
   let(:gems_and_minerals) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
   let(:dead_sea_scrolls) { Exhibit.new({name: "Dead Sea Scrolls", cost: 10}) }
   let(:imax) { Exhibit.new({name: "IMAX",cost: 15}) }
+  let(:patron_1) { Patron.new("Bob", 20)}
+  let(:patron_2) { Patron.new("Sally", 20)}
 
   it '#name' do
     expect(dmns.name).to eq("Denver Museum of Nature and Science")
@@ -19,5 +21,14 @@ RSpec.describe Museum do
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
     expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+  it '#recommend_exhibits' do
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("IMAX")
+
+    expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+    expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
   end
 end
