8896fa88f56eaced96c5d63876853030b090871c
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-27 10:28:30 -0400

    add initialize tests

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+RSpec.describe Museum do
+  let(:dmns) { described_class.new("Denver Museum of Nature and Science") }
+
+  it '#name' do
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+  end
+
+  it '#exhibits' do
+    expect(dmns.exhibits).to eq([])
+  end
+end
