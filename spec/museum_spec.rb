6aed8abcf2044f854152015efb181428cac77b34
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-27 11:39:30 -0400

    add test for #ticket_lottery_contestants

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Museum do
   let(:gems_and_minerals) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
   let(:dead_sea_scrolls) { Exhibit.new({name: "Dead Sea Scrolls", cost: 10}) }
   let(:imax) { Exhibit.new({name: "IMAX",cost: 15}) }
-  let(:patron_1) { Patron.new("Bob", 20)}
+  let(:patron_1) { Patron.new("Bob", 0)}
   let(:patron_2) { Patron.new("Sally", 20)}
   let(:patron_3) { Patron.new("Johnny", 5)}
 
@@ -51,7 +51,7 @@ RSpec.describe Museum do
     dmns.admit(patron_2)
     dmns.admit(patron_3)
 
-    expect(dmns.patrons).to eq(patron_1, patron_2, patron_3)
+    expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
   end
 
   it '#patrons_by_exhibit_interest' do
@@ -59,12 +59,14 @@ RSpec.describe Museum do
     patron_1.add_interest("Gems and Minerals")
     patron_2.add_interest("Dead Sea Scrolls")
     patron_3.add_interest("Dead Sea Scrolls")
-
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
     dmns.admit(patron_1)
     dmns.admit(patron_2)
     dmns.admit(patron_3)
 
-    expect(patrons_by_exhibit_interest).to eq(
+    expect(dmns.patrons_by_exhibit_interest).to eq(
       {
         gems_and_minerals => [patron_1],
         dead_sea_scrolls => [patron_1, patron_2, patron_3],
@@ -72,4 +74,10 @@ RSpec.describe Museum do
       }
     )
   end
+
+  it '#ticket lottery contestants' do
+    expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq([patron_1, patron_3])
+  end
+
+  
 end
